c6f8740d0574fa65a1a51ba56039233a55dd91d8
sandy-S19 <sangyaofficial@gmail.com>
2020-05-11 14:53:57 +0530

    add all files

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <Search/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import Search from './components/Search'
+export default {
+  name: 'App',
+  components: {
+    Search
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,51 @@
+<template>
+
+  <div id="Search">
+  <marquee>
+    <h1>Type the number for finding out the cube:</h1>
+</marquee>
+	<input v-model="query">
+	  <button v-on:click="handleSubmit">Enter</button>
+    
+	<p> </p>
+</div>
+</template>
+<script>
+export default {
+  name: 'Search',
+  data () {
+    return {
+      query:''
+  }
+ },
+ methods:{
+ handleSubmit: 
+ 
+ function(){
+ if(isNaN(this.query)){
+ alert("not a valid number");
+ }
+ else
+ alert("the cube of the number is   " +this.query*this.query*this.query);
+ 
+ }
+ }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight:  very large;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: green;
+}
+</style>
